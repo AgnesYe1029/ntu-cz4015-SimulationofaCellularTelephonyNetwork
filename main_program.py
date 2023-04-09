c596f2cee2a14414bbf5779f57b1ed91f85365a9
AgnesYe1029 <ziyuan1029@gmail.com>
2023-04-09 22:07:51 +0800

    output analysis

--- main_program.py
+++ main_program.py
@@ -4,9 +4,10 @@ from input_generator import InputGenerator
 from event import *
 from heapq import heappush, heappop
 import system_constants as const
+import pickle
 
 
-class MainProgram:
+class SimulationProgram:
     def __init__(self) -> None:
         # clock and time of the last event set to 0
         self.time_of_the_last_event = 0
@@ -25,19 +26,24 @@ class MainProgram:
         self.finished_calls = 0
 
     def initialize(self):
+        self.time_of_the_last_event = 0
+        self.clock = 0
+        self.future_event_list = []
+        self.available_channels = [10] * 20
+        self.total_number_of_calls = 0
+        self.dropped_calls = 0
+        self.blocked_calls = 0
+        self.finished_calls = 0
         # create the first call initiation event and insert to list.
         self.schedule_call_initiation_event(is_first=True)
-        print("upon initialization the future event list: ", self.future_event_list)
 
-    def start_simulation(self):
+    def main_program(self, experiment_label="0"):
+        qos_stats = []
         self.initialize()
+        clock_counter = 0
         # simulate calls
-        while self.finished_calls < 100000:
+        while self.clock < 140000:
             event = heappop(self.future_event_list)[1]
-            # print("======================")
-            # print("time: ", event.time)
-            # print("event: ", event)
-            # print("channel avail: ", self.available_channels)
             self.clock = event.time  # advance the clock
             # event handling corresponding to the type of event
             if isinstance(event, CallInitiationEvent):
@@ -46,11 +52,67 @@ class MainProgram:
                 self.handle_call_handover(event)
             elif isinstance(event, CallTerminationEvent):
                 self.handle_call_termination(event)
+            # qos
+            if clock_counter < 360000:
+                qos_stats.append([clock_counter, self.clock,
+                                  self.blocked_calls / self.finished_calls if self.finished_calls != 0 else 0,
+                                  self.dropped_calls / self.finished_calls if self.finished_calls != 0 else 0])
+            clock_counter += 1
+
+        with open("output_analysis/output_data/qos_" + str(const.FCA_NUMBER) + "_" + experiment_label + ".pkl", "wb") as fout:
+            pickle.dump(qos_stats, fout)
+
         print("======================")
         print("num_of_calls: ", self.total_number_of_calls)
         print("finished_calls: ", self.finished_calls)
         print("blocked_calls: ", self.blocked_calls)
         print("dropped_calls: ", self.dropped_calls)
+        print("event handled: ", clock_counter)
+
+    def simulate_to_find_warmup(self, replications):
+        for i in range(replications):
+            self.main_program(str(i))
+
+    def main_program_with_warm_up(self, warm_up_period):
+        is_warmed_up = False
+        self.initialize()
+        # simulate calls
+        while self.clock < 140000:
+            event = heappop(self.future_event_list)[1]
+            self.clock = event.time  # advance the clock
+            if self.clock > warm_up_period and not is_warmed_up:
+                is_warmed_up = True
+                self.clear_counters()
+            # event handling corresponding to the type of event
+            if isinstance(event, CallInitiationEvent):
+                self.handle_call_initiation(event)
+            elif isinstance(event, CallHandoverEvent):
+                self.handle_call_handover(event)
+            elif isinstance(event, CallTerminationEvent):
+                self.handle_call_termination(event)
+
+        blocked_percentage = self.blocked_calls / self.finished_calls
+        dropped_percentage = self.dropped_calls / self.finished_calls
+
+        print("======================")
+        print("blocked_percentage: ", blocked_percentage)
+        print("dropped_percentage: ", dropped_percentage)
+
+        return blocked_percentage, dropped_percentage
+
+    def simulate_with_warm_up(self, replications, warm_up_period):
+        percentages = []
+        for i in range(replications):
+            b, d = self.main_program_with_warm_up(warm_up_period)
+            percentages.append([i, b, d])
+        with open("output_analysis/output_data/qos_" + str(const.FCA_NUMBER) + "_percentages.pkl", "wb") as fout:
+            pickle.dump(percentages, fout)
+        return percentages
+
+    def clear_counters(self):
+        self.finished_calls = 0
+        self.blocked_calls = 0
+        self.dropped_calls = 0
 
     def handle_call_initiation(self, event):
         # schedule next call event
@@ -60,7 +122,7 @@ class MainProgram:
 
         # process the current call event
         curr_station = event.initiation_station
-        if self.available_channels[curr_station] > 1:
+        if self.available_channels[curr_station] > const.FCA_NUMBER:
             self.available_channels[curr_station] -= 1
             time_stay_in_this_cell = (const.CELL_DIAMETER - event.initiation_position) / event.car_speed \
                 if event.car_direction == 1 else event.initiation_position / event.car_speed
@@ -145,10 +207,10 @@ class MainProgram:
         next_call_init_event = CallInitiationEvent(next_call_init_time, next_car_speed, next_call_init_station,
                                                    next_call_init_position, next_call_duration, next_car_direction)
         if is_first:
-            self.clock = next_call_init_event
+            self.clock = next_call_init_event.time
         heappush(self.future_event_list, (next_call_init_time, next_call_init_event))
 
 
 if __name__ == "__main__":
-    main = MainProgram()
-    main.start_simulation()
+    simulator = SimulationProgram()
+    simulator.simulate_with_warm_up(20, 60000)
